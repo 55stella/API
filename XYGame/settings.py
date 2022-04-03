7a76ace6e48f8daa155c983efb3a8ae6a0081323
55stella <stellarichards7@gmail.com>
2022-04-03 22:28:27 +0100

    .env

--- XYGame/settings.py
+++ XYGame/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+import os
+from dotenv.main import find_dotenv
+load_dotenv(find_dotenv())
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w)5p&x#%3q*q*j2f&f2a34n&p$)+jsy_zed=5vm53i0d^3-_78'
+SECRET_KEY = os.getenv('DJANGO_SECRETE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
